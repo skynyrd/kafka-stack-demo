f5a387af788c6716cdda0abe1b6426efc0a6b116
Anil Surmeli <anil.surmeli@gmail.com>
2019-10-20 18:37:12 +1100

    initializes product consumer

---
+++ ProductConsumer/ProductConsumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace ProductConsumer
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            var conf = new ConsumerConfig
+            { 
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+            c.Subscribe("output-test");
+
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = c.Consume(cts.Token);
+                        Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                c.Close();
+            }
+        }
+    }
+}
