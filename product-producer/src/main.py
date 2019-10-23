d475e3e437efde6d2927464dc4aa63f78122fae1
Anil Selim Surmeli <anil.surmeli@gmail.com>
2019-10-23 23:50:17 +1100

    removes unnecessary topic creation block from producer

--- product-producer/src/main.py
+++ product-producer/src/main.py
@@ -6,19 +6,15 @@ from kafka.admin import KafkaAdminClient, NewTopic
 from kafka import KafkaProducer
 
 from src.Product import Product
-
+print("We're ready")
 
 admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092", client_id='test')
-# topic_list = [NewTopic(name="test_topic", num_partitions=1, replication_factor=1)]
-# admin_client.create_topics(new_topics=topic_list, validate_only=False)
-
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: ujson.dumps(v).encode('utf-8'))
 
 
 def job():
-    for i in range(250):
-        product = Product.load_random()
-        producer.send(topic="test-topic", value=product.to_kafka_model())
+    product = Product.load_random()
+    producer.send(topic="test-topic", value=product.to_kafka_model())
 
 
 schedule.every(1).second.do(job)
